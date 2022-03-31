bf99c46652e88e679956f30c0606a4d4a7e9d96f
Thelma_Lim <dusgud711@ewhain.net>
2022-03-31 17:14:18 +0900

    RDB設計研修で設計したテーブルのうち、必要なテーブルをローカル環境のMySQLに作成しました。

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE article_service;
+
 USE article_service;
 
 CREATE TABLE editors (
@@ -12,11 +13,37 @@ CREATE TABLE editors (
 
 CREATE TABLE articles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    editor_id BIGINT,
+    editor_id BIGINT NOT NULL,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     thumbnail_id BIGINT,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_editors_articles_id FOREIGN KEY (editor_id) REFERENCES editors (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE images (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    article_id BIGINT NOT NULL,
+    src VARCHAR(2048),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_articles_images_id FOREIGN KEY (article_id) REFERENCES articles (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE tags (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    tagname VARCHAR(45) UNIQUE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE article_tags (
+    article_id BIGINT NOT NULL,
+    tag_id BIGINT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (article_id, tag_id),
+    CONSTRAINT fk_articles_article_tags_id FOREIGN KEY (article_id) REFERENCES articles (id),
+    CONSTRAINT fk_tags_article_tags_id FOREIGN KEY (tag_id) REFERENCES tags (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
