6e7f91abae57288a50e288b33ca944e80042fdef
yeonhyunglim <dusgud711@naver.com>
2022-05-20 18:20:10 +0900

    tagsテーブルにデータをINSERTするqueryを作成しました。

---
+++ db/tags.sql
@@ -0,0 +1,13 @@
+USE article_service;
+
+INSERT INTO `tags` (`id`, `tagname`) VALUES (1, '総合');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (2, 'テクノロジー');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (3, 'モバイル');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (4, 'アプリ');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (5, 'エンタメ');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (6, 'ビューティー');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (7, 'ファッション');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (8, 'ライフスタイル');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (9, 'ビジネス');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (10, 'グルメ');
+INSERT INTO `tags` (`id`, `tagname`) VALUES (11, 'スポーツ');
