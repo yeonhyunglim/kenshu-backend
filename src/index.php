2c87363b58fab167c65f6413dedeaad1833f16cf
yeonhyunglim <dusgud711@naver.com>
2022-05-23 15:34:15 +0900

    bootstrapを書き換えました。

--- src/index.php
+++ src/index.php
@@ -12,7 +12,7 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.8.1/css/bootstrap-select.css">
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css">
     <link rel="stylesheet" href="./public/assets/css/style.css">
 </head>
 
@@ -43,8 +43,8 @@
             <!-- tags  -->
             <div class="form-group">
                 <label>タグ</label>
-                <select class="selectpicker form-control" name="tags[]" multiple required>
-                    <option value="総合" selected="selected">総合</option>
+                <select class="selectpicker form-control is-invalid" name="tags[]" multiple required>
+                    <option value="総合">総合</option>
                     <option value="テクノロジー">テクノロジー</option>
                     <option value="モバイル">モバイル</option>
                     <option value="アプリ">アプリ</option>
@@ -73,8 +73,9 @@
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.8.1/js/bootstrap-select.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+
 </body>
 
 </html>
